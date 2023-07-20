d8df99cbf75799aeec077d56c811ba9d8f1d5361
Mark Faust <markfaust123@gmail.com>
2023-07-20 11:13:52 -0400

    Created the question.dart class.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,17 +27,22 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+
+  List<Question> questions = [
+    Question(
+        question: 'You can lead a cow down stairs but not up stairs.',
+        answer: false),
+    Question(
+        question: 'Approximately one quarter of human bones are in the feet.',
+        answer: true),
+    Question(question: 'A slug\'s blood is green.', answer: true),
   ];
-  List<bool> answers = [false, true, true];
+
   int questionNumber = 0;
 
   void verifyAnswer(answer) {
     if (scoreKeeper.length >= 15) scoreKeeper.clear();
-    bool correct = answer == answers[questionNumber];
+    bool correct = answer == questions[questionNumber].questionAnswer;
     if (correct) {
       scoreKeeper.add(
         Icon(
@@ -67,7 +73,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -130,9 +136,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText = '';
+  bool questionAnswer = false;
+
+  Question({String question = '', bool answer = false}) {
+    questionText = question;
+    questionAnswer = answer;
+  }
+}
